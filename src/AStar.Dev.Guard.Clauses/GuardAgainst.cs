6f145615e2068568311b56ce44ca68e273f4a3c8
Jason Barden <jason.barden@outlook.com>
2025-04-26 06:07:32 +0100

    Add GuardAgainst.Negative

--- src/AStar.Dev.Guard.Clauses/GuardAgainst.cs
+++ src/AStar.Dev.Guard.Clauses/GuardAgainst.cs
@@ -1,4 +1,6 @@
-﻿namespace AStar.Dev.Guard.Clauses;
+﻿using System.Numerics;
+
+namespace AStar.Dev.Guard.Clauses;
 
 /// <summary>
 ///     The root <seealso href="GuardAgainst"></seealso> class.
@@ -11,7 +13,7 @@ public static class GuardAgainst
     /// <typeparam name="T">
     ///     Specifies the generic object to check for null.
     /// </typeparam>
-    /// <param name="object">
+    /// <param name="objectToCheck">
     ///     The object to check for null.
     /// </param>
     /// <returns>
@@ -20,6 +22,19 @@ public static class GuardAgainst
     /// <exception cref="ArgumentNullException">
     ///     Thrown when the object is, in fact, null.
     /// </exception>
-    public static T Null<T>(T @object) =>
-        @object is null ? throw new ArgumentNullException(nameof(@object)) : @object;
+    public static T Null<T>(T objectToCheck)
+    {
+        return objectToCheck is null ? throw new ArgumentNullException(nameof(objectToCheck)) : objectToCheck;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="objectToCheck"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static INumber<T> Negative<T>(T objectToCheck) where T : INumber<T>
+    {
+        return objectToCheck < T.Zero ? throw new ArgumentOutOfRangeException(nameof(objectToCheck)) : objectToCheck;
+    }
 }
